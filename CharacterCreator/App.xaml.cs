c7e1e104bf4f01f37a96b7e7486b22f06342dfc1
Sichlan IV <hyschommers@gmx.de>
2021-07-14 22:08:59 +0200

    #5 - localization

--- CharacterCreator/App.xaml.cs
+++ CharacterCreator/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +14,29 @@ namespace CharacterCreator
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            string localization = "en-US";
+
+#if (DEBUG == false)
+            if (String.IsNullOrEmpty(CharacterCreator.Properties.Settings.Default.StandardLocalization))
+            {
+                string sysLang = CultureInfo.CurrentCulture.Name;
+                if(MessageBox.Show($"You have not set a standard localization for this app.\nYour system language is set to {CultureInfo.CurrentCulture.DisplayName}.\nDo you want to set your default language for this application to {CultureInfo.CurrentCulture.DisplayName}?", "Select language", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    localization = sysLang;
+                    CharacterCreator.Properties.Settings.Default.StandardLocalization = sysLang;
+                }
+            }
+            else
+            {
+                localization = CharacterCreator.Properties.Settings.Default.StandardLocalization;
+            }
+#endif
+
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new CultureInfo(localization);
+            System.Threading.Thread.CurrentThread.CurrentCulture = new CultureInfo(localization);
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(System.Windows.Markup.XmlLanguage.GetLanguage(new CultureInfo(localization).IetfLanguageTag)));
+        }
     }
 }
